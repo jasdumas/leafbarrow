b14c874cd13a56718a3f5e6c7b25178245a2940c
Jenna Daly <jenna.m.daly@gmail.com>
2019-01-08 16:52:01 -0500

    add auto slideshow code

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,4 +1,5 @@
-var slideIndex = 1;
+//click to change pic
+/*var slideIndex = 1;
   showSlides(slideIndex);
   
   function plusSlides(n) {
@@ -24,3 +25,22 @@ var slideIndex = 1;
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
   }
+*/
+
+//automatic slide show
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
